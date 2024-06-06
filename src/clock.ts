959aa7bb67e9563711b8831567428ceaf17b8576
Akira FUNAI <funai@penguin.lxd>
2024-06-06 20:24:59 +0900

    cosmetics

--- src/clock.ts
+++ src/clock.ts
@@ -8,22 +8,22 @@ const agent = new BskyAgent({
 });
 
 const main = async () => {
-    agent.login({
-      identifier: process.env.BLUESKY_USERNAME!,
-      password: process.env.BLUESKY_PASSWORD!
-    }).then(_ => {
-      fetchWeatherText({
-        locationName: '東京',
-        latitude: 35.7068,
-        longitude: 139.7245,
-      }).then(text => {
-        agent.post({
-          text: text
-        }).then(({uri, cid}) => {
-          console.log(`Posted: ${uri}`);
-        });
+  agent.login({
+    identifier: process.env.BLUESKY_USERNAME!,
+    password: process.env.BLUESKY_PASSWORD!
+  }).then(_ => {
+    fetchWeatherText({
+      locationName: '東京',
+      latitude: 35.7068,
+      longitude: 139.7245,
+    }).then(text => {
+      agent.post({
+        text: text
+      }).then(({uri, cid}) => {
+        console.log(`Posted: ${uri}`);
       });
     });
+  });
 };
 
 new CronJob(process.env.CRONTAB!, main).start();
