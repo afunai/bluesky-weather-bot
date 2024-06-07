5e9265adc4ab26a1f8a9b20b8717d3a0675c26a3
Akira FUNAI <funai@penguin.lxd>
2024-06-07 21:29:21 +0900

    code2emoji()

--- src/openmeteo/weather-code.ts
+++ src/openmeteo/weather-code.ts
@@ -68,3 +68,51 @@ export const code2str = (weather_code: number = 99): string => {
       return '不明';
   }
 };
+
+export const code2emoji = (weather_code: number = 99): string => {
+  switch (weather_code) {
+    case 0:
+      return '☀';
+    case 1:
+      return '🌤';
+    case 2:
+      return '🌥';
+    case 3:
+      return '☁';
+    case 45:
+    case 48:
+      return '🌫';
+    case 51:
+    case 53:
+    case 55:
+    case 56:
+    case 57:
+    case 61:
+      return '🌂';
+    case 63:
+      return '☂';
+    case 65:
+      return '☔';
+    case 66:
+    case 67:
+      return '🌨';
+    case 71:
+    case 73:
+    case 75:
+    case 77:
+      return '☃';
+    case 80:
+    case 81:
+    case 82:
+      return '🌦';
+    case 85:
+    case 86:
+      return '🌨';
+    case 95:
+    case 96:
+    case 99:
+      return '🌩';
+    default:
+      return '　';
+  }
+};
